f6eb9cef393fd3c8a4187f7434781da072ebd632
Rahul Kumar <rahulkumar151995@gmail.com>
2020-11-16 23:31:53 +0530

    minor changes

--- src/environments/prod.env.ts
+++ src/environments/prod.env.ts
@@ -1,7 +1,7 @@
 import { Environment } from './env'
 
 export const ProdEnvironment: Environment = {
-    db_url: "mongodb+srv://demo:demo@cluster0.cjcjs.mongodb.net/postWeb?retryWrites=true&w=majority",
+    db_url: "mongodb+srv://demo:demo@cluster0.cjcjs.mongodb.net/erm?retryWrites=true&w=majority",
     jwt_secret: "prodSecret",
     emailjs_com: {
         service_id: 'service_k65149f',
